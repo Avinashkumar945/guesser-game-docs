17486147356a4e038171b4105f720dc2986d5593
avinash <avinash732004kumar@gmail.com>
2025-05-10 17:01:54 +0530

    changes

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"guesser"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"player"},{"p":"<Unnamed>","l":"umpire"}];updateSearchResults();
